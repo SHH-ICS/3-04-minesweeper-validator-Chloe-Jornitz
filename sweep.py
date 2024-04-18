8bc0e3d33b7046438519ec81463e37952d15c646
Chloe-Jornitz <159150248+Chloe-Jornitz@users.noreply.github.com>
2024-04-18 12:26:07 +0000

    while loop

--- sweep.py
+++ sweep.py
@@ -4,26 +4,30 @@
 # Return value should be a string that says either Valid or Invalid (with some hints as to why it's invlaid) 
 def validate( block_data ):
   bomb_count = 0
+  result = ""
   # Check whether the centre block is a bomb, a number, or an invalid input
   # Skip bombs, send an error on invalid input, verify numbers
-  for i in range (3):
-    for j in range (3):
-      if block_data [i][j] == -1:
-        bomb_count = bomb_count + 1
-  if block_data [1][1] == -1:
-    return "This block is a bomb."
-  if block_data [1][1] == bomb_count:
-    if block_data [1][1] == 1:
-      return 'Valid. There is '+ str(bomb_count)+' bomb around this block.'
-    if block_data [1][1] != 1:
-      return 'Valid. There are '+ str(bomb_count)+' bombs around this block.'
-  if block_data [1][1] != bomb_count:
-    return 'Invalid. There are '+ str(bomb_count)+' bombs around this block.'
+  while (1):
+    if block_data [1][1] == -1:
+        result = "This block is a bomb."
+        break
+    for i in range (3):
+      for j in range (3):
+        if block_data [i][j] == -1:
+          bomb_count = bomb_count + 1
+    if block_data [1][1] == bomb_count:
+      if block_data [1][1] == 1:
+        result = 'Valid. There is '+ str(bomb_count)+' bomb around this block.'
+      if block_data [1][1] != 1:
+        result = 'Valid. There are '+ str(bomb_count)+' bombs around this block.'
+    if block_data [1][1] != bomb_count:
+      result = 'Invalid. There are '+ str(bomb_count)+' bombs around this block.'
+  return result
 
 
 grid = [
   [-1,1,0],
-  [-1,2,0],
+  [-1,-1,0],
   [0,0,0]
 ]
 print (validate(grid))
