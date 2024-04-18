3a9cc10f89df3d240cb3ea9fa4fce285953cc5ac
Chloe-Jornitz <159150248+Chloe-Jornitz@users.noreply.github.com>
2024-04-18 13:30:46 +0000

    try except

--- sweep.py
+++ sweep.py
@@ -2,39 +2,43 @@
 # block_data is a two dimensional array containing the data from a 3 x 3 grid of squares
 # We are assuming that we are only checking interior blocks for now
 # Return value should be a string that says either Valid or Invalid (with some hints as to why it's invlaid) 
-def validate( block_data ):
-  bomb_count = 0
-  result = ""
-  while (1):
-  # Check whether the centre block is a bomb, a number, or an invalid input
-  # Skip bombs, send an error on invalid input, verify numbers
-    if block_data [1][1] == -1:
-        result = "This block is a bomb."
-        break
-    for i in range (3):
-      for j in range (3):
-        if block_data [i][j] == -1:
-          bomb_count = bomb_count + 1
-    if block_data [1][1] == bomb_count:
-      if bomb_count == 1:
-        result = 'Valid. There is ' + str(bomb_count) + ' bomb around this block. The number in this block is ' + str(block_data[1][1]) + '. The number in the block is equal to the number of surrounding bombs.'
-        break
-      if bomb_count != 1:
-        result = 'Valid. There are '+ str(bomb_count)+' bombs around this block. The number in this block is ' + str(block_data[1][1]) + '. The number in the block is equal to the number of surrounding bombs.'
-        break
-    if block_data [1][1] != bomb_count:
-      if bomb_count == 1:
-        result = 'Invalid. There is '+ str(bomb_count)+' bomb around this block. The number in this block is ' + str(block_data[1][1]) + '. The number in the block is not equal to the number of surrounding bombs.'
-        break
-      if bomb_count != 1:
-        result = 'Invalid. There are '+ str(bomb_count)+' bombs around this block. The number in this block is ' + str(block_data[1][1]) + '. The number in the block is not equal to the number of surrounding bombs.'
-        break
-  return result
+try:
+  def validate( block_data ):
+    bomb_count = 0
+    result = ""
+    while (1):
+    # Check whether the centre block is a bomb, a number, or an invalid input
+    # Skip bombs, send an error on invalid input, verify numbers
+      if block_data [1][1] == -1:
+          result = "This block is a bomb."
+          break
+      for i in range (3):
+        for j in range (3):
+          if block_data [i][j] == -1:
+            bomb_count = bomb_count + 1
+      if block_data [1][1] == bomb_count:
+        if bomb_count == 1:
+          result = 'Valid. There is ' + str(bomb_count) + ' bomb around this block. The number in this block is ' + str(block_data[1][1]) + '. The number in the block is equal to the number of surrounding bombs.'
+          break
+        if bomb_count != 1:
+          result = 'Valid. There are '+ str(bomb_count)+' bombs around this block. The number in this block is ' + str(block_data[1][1]) + '. The number in the block is equal to the number of surrounding bombs.'
+          break
+      if block_data [1][1] != bomb_count:
+        if bomb_count == 1:
+          result = 'Invalid. There is '+ str(bomb_count)+' bomb around this block. The number in this block is ' + str(block_data[1][1]) + '. The number in the block is not equal to the number of surrounding bombs.'
+          break
+        if bomb_count != 1:
+          result = 'Invalid. There are '+ str(bomb_count)+' bombs around this block. The number in this block is ' + str(block_data[1][1]) + '. The number in the block is not equal to the number of surrounding bombs.'
+          break
+    return result
 
-grid = [
-  [1,1,1],
-  [1,1,0],
-  [-1,0,1]
-]
-  
-print (validate(grid))
+  grid = [
+    [1,o,-1],
+    [1,1,0],
+    [-1,0,1]
+  ]
+    
+  print (validate(grid))
+except:
+  print ("Error!")
+  print ("The value in this block, or in any surrounding blocks is not a number. Please fill the grid with only numbers.")
